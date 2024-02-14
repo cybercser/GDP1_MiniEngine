5ec58d7f7490e5d895d778a434939195a4ad8256
Prathap kumar <prathapkumar.bobby@gmail.com>
2024-02-14 15:01:33 -0500

    added player and weapon classes

---
+++ App/Source/GameObjects/player.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <Core/game_object.h>
+
+#include "weapon.h"
+
+namespace Game {
+
+enum WeaponSelection { PRIMARY_WEAPON, SECONDARY_WEAPON, MELEE_WEAPON };
+
+class Player : public gdp1::GameObject {
+
+public:
+
+    Player(gdp1::Scene* scn, const gdp1::GameObjectDesc& desc);
+    Player(gdp1::Scene* scn, const std::string& name);
+
+    // Set 
+    void SetHealth(int health);
+    void TakeDamage(int damage);
+
+    // Set primary, secondary, and melee weapons
+    void SetPrimaryWeapon(Weapon* primary_weapon);
+    void SetSecondaryWeapon(Weapon* secondary_weapon);
+    void SetMeleeWeapon(Weapon * melee_weapon);
+
+    // Change Weapon Selection
+    void ChangeWeapon(WeaponSelection selection);
+
+    // Use actions
+    void Shoot();
+    void Collect();
+    void Sprint();
+    void Reload();
+
+private:
+    // Additional player-specific attributes and methods can be added here
+
+    int health;
+    float movement_speed;
+
+    WeaponSelection current_selected_weapon;
+
+    Weapon* primary_weapon;
+    Weapon* secondary_weapon;
+    Weapon* melee_weapon;
+
+};
+
+}
