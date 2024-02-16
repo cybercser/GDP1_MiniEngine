3dff07af9673d4cd2d0f719c53040da4a27c87ca
cybercser <cybercser@gmail.com>
2024-02-16 13:06:14 -0500

    add BOX and CAPSULE collider, fix MESH collider

--- Engine/Source/Runtime/Physics/collider.cpp
+++ Engine/Source/Runtime/Physics/collider.cpp
@@ -1,117 +1,156 @@
 #include "collider.h"
 
+#include "Core/core.h"
 #include "Core/game_object.h"
 #include "Render/model.h"
 
+using namespace glm;
+
 namespace gdp1 {
 
+//******************************************************************************
+// SPHERE COLLIDER
+//******************************************************************************
 #pragma region SphereCollider
 Bounds SphereCollider::GetBounds(const glm::vec3& pos, const glm::vec3& scale, const glm::quat& orient) const {
-    return Bounds{pos, glm::vec3{radius, radius, radius}};
+    return Bounds{pos, vec3{radius, radius, radius} * 2.0f * scale};
 }
 
 Bounds SphereCollider::GetBounds(const glm::mat4& transMat) const {
-    glm::vec3 pos = transMat[3];
-    glm::vec3 scale = glm::vec3{glm::length(transMat[0]), glm::length(transMat[1]), glm::length(transMat[2])};
-    return Bounds{pos, glm::vec3{radius, radius, radius} * scale};
+    vec3 pos = transMat[3];
+    vec3 scale = vec3{length(transMat[0]), length(transMat[1]), length(transMat[2])};
+    return Bounds{pos, vec3{radius, radius, radius} * 2.0f * scale};
 }
 
 Bounds SphereCollider::GetBounds() const {
-    return Bounds{glm::vec3{0.0f, 0.0f, 0.0f}, glm::vec3{radius, radius, radius}};
+    GLCORE_ASSERT(gameObject, "GameObject must not be null");
+    GLCORE_ASSERT(gameObject->transform, "GameObject must have a transform component");
+    return GetBounds(gameObject->transform->WorldMatrix());
 }
 #pragma endregion
 
-#if 0
+//******************************************************************************
+// BOX COLLIDER
+//******************************************************************************
 #pragma region BoxCollider
-Bounds BoxCollider::GetBounds(const glm::vec3& pos, const glm::vec3& lossyScale, const glm::quat& orient) const {
+Bounds BoxCollider::GetBounds(const glm::vec3& pos, const glm::vec3& scale, const glm::quat& orient) const {
+    // calculate the transform matrix
+    mat4 transMat = mat4(1.0f);
+    transMat = translate(transMat, pos);
+    transMat *= mat4(orient);
+    transMat = glm::scale(transMat, scale);
+    return GetBounds(transMat);
 }
 
 Bounds BoxCollider::GetBounds(const glm::mat4& transMat) const {
+    vec3 min = center - size / 2.0f;
+    vec3 max = center + size / 2.0f;
+    vec3 transformedMin = vec3(transMat * vec4(min, 1.0f));
+    vec3 transformedMax = vec3(transMat * vec4(max, 1.0f));
+
+    return Bounds{transformedMin, transformedMax};
 }
 
 Bounds BoxCollider::GetBounds() const {
+    GLCORE_ASSERT(gameObject, "GameObject must not be null");
+    GLCORE_ASSERT(gameObject->transform, "GameObject must have a transform component");
+    return GetBounds(gameObject->transform->WorldMatrix());
 }
 #pragma endregion
-#endif
 
+//******************************************************************************
+// CAPSULE COLLIDER
+//******************************************************************************
+#pragma region CapsuleCollider
+Bounds CapsuleCollider::GetBounds(const glm::vec3& pos, const glm::vec3& scale, const glm::quat& orient) const {
+    // calculate the transform matrix
+    mat4 transMat = mat4(1.0f);
+    transMat = translate(transMat, pos);
+    transMat *= mat4(orient);
+    transMat = glm::scale(transMat, scale);
+    return GetBounds(transMat);
+}
+
+Bounds CapsuleCollider::GetBounds(const glm::mat4& transMat) const {
+    float size = GetHeight();
+    vec3 min = center - size / 2.0f;
+    vec3 max = center + size / 2.0f;
+    vec3 transformedMin = vec3(transMat * vec4(min, 1.0f));
+    vec3 transformedMax = vec3(transMat * vec4(max, 1.0f));
+
+    return Bounds{transformedMin, transformedMax};
+}
+
+Bounds CapsuleCollider::GetBounds() const {
+    GLCORE_ASSERT(gameObject, "GameObject must not be null");
+    GLCORE_ASSERT(gameObject->transform, "GameObject must have a transform component");
+    return GetBounds(gameObject->transform->WorldMatrix());
+}
+#pragma endregion
+
+//******************************************************************************
+// MESH COLLIDER
+//******************************************************************************
 #pragma region MeshCollider
-MeshCollider::MeshCollider(GameObject* pObject)
-    : object(pObject) {
-    centerOfMass = glm::vec3(0.0);
+MeshCollider::MeshCollider(GameObject* pObject) {
+    center = glm::vec3(0.0);
+    gameObject = pObject;
 
     // copy vertices
-    Model* model = object->model;
+    Model* model = gameObject->model;
     if (model == nullptr) {
         return;  // ignore empty game object
     }
 
     // copy mesh's transform so that the collider coincides with the mesh
     // combine all meshes into one
-    // #TODO: simplified collider mesh (convex hull)
-    glm::mat4 transMat = object->transform->WorldMatrix();
+    // #TODO: simplify collider mesh (convex hull)
     unsigned int indexOffset = 0;
+    m_Mesh.vertices.reserve(model->GetVertexCount());
     for (auto& mesh : model->meshes) {
         for (auto& vertex : mesh.vertices) {
             ColliderVertex cv;
-            cv.position = glm::vec3(glm::vec4(vertex.position, 1.0f) * transMat);
+            cv.position = vertex.position;
             cv.texCoords = vertex.texCoords;
-            hull_.vertices.push_back(cv);
+            cv.normal = vertex.normal;
+            m_Mesh.vertices.push_back(cv);
         }
-        for (auto& index : mesh.indices) {
-            hull_.indices.push_back(index + indexOffset);
+
+        for (unsigned int i = 0; i < mesh.indices.size(); i += 3) {
+            ColliderTriangle ct;
+            ct.indices[0] = mesh.indices[i] + indexOffset;
+            ct.indices[1] = mesh.indices[i + 1] + indexOffset;
+            ct.indices[2] = mesh.indices[i + 2] + indexOffset;
+            m_Mesh.triangles.push_back(ct);
         }
         indexOffset += mesh.vertices.size();
     }
-    SetupMesh();
 
-    // transform bounds
-    glm::vec3 min = model->bounds.GetMin();
-    glm::vec3 max = model->bounds.GetMax();
-    glm::vec3 transformedMin = glm::vec3(transMat * glm::vec4(min, 1.0f));
-    glm::vec3 transformedMax = glm::vec3(transMat * glm::vec4(max, 1.0f));
-
-    this->bounds_.SetMinMax(transformedMin, transformedMax);
+    this->m_OriginalBounds = model->bounds;
 }
 
 Bounds MeshCollider::GetBounds(const glm::vec3& pos, const glm::vec3& scale, const glm::quat& orient) const {
-    glm::mat4 transMat =
-        glm::translate(glm::mat4(1.0f), pos) * glm::toMat4(orient) * glm::scale(glm::mat4(1.0f), scale);
-    return this->GetBounds(transMat);
+    mat4 transMat = mat4(1.0f);
+
+    transMat = translate(transMat, pos);
+    transMat *= mat4(orient);
+    transMat = glm::scale(transMat, scale);
+
+    return GetBounds(transMat);
 }
 
 Bounds MeshCollider::GetBounds(const glm::mat4& transMat) const {
-    glm::vec3 min = bounds_.GetMin();
-    glm::vec3 max = bounds_.GetMax();
-    glm::vec3 transformedMin = glm::vec3(transMat * glm::vec4(min, 1.0f));
-    glm::vec3 transformedMax = glm::vec3(transMat * glm::vec4(max, 1.0f));
-    Bounds result;
-    result.SetMinMax(transformedMin, transformedMax);
-    return result;
+    vec3 min = m_OriginalBounds.GetMin();
+    vec3 max = m_OriginalBounds.GetMax();
+    vec3 transformedMin = vec3(transMat * vec4(min, 1.0f));
+    vec3 transformedMax = vec3(transMat * vec4(max, 1.0f));
+    return Bounds{transformedMin, transformedMax};
 }
 
 Bounds MeshCollider::GetBounds() const {
-    return bounds_;
-}
-
-void MeshCollider::SetupMesh() {
-    glGenVertexArrays(1, &hull_.VAO);
-    glGenBuffers(1, &hull_.VBO);
-
-    glBindVertexArray(hull_.VAO);
-
-    // load data into vertex buffers
-    glBindBuffer(GL_ARRAY_BUFFER, hull_.VBO);
-    glBufferData(GL_ARRAY_BUFFER, hull_.vertices.size() * sizeof(ColliderVertex), &hull_.vertices[0], GL_STATIC_DRAW);
-
-    // set the vertex attribute pointers
-    // vertex Positions
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(ColliderVertex), (void*)0);
-    // vertex texture coords
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(ColliderVertex), (void*)offsetof(ColliderVertex, texCoords));
-
-    glBindVertexArray(0);
+    GLCORE_ASSERT(gameObject, "GameObject must not be null");
+    GLCORE_ASSERT(gameObject->transform, "GameObject must have a transform component");
+    return GetBounds(gameObject->transform->WorldMatrix());
 }
 
 #pragma endregion
