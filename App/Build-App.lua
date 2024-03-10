97a8832405811d761e72b4f5c54f37beb821e768
Prathap kumar <prathapkumar.bobby@gmail.com>
2024-03-10 01:04:00 -0500

    Fixed Build-App lua script

--- App/Build-App.lua
+++ App/Build-App.lua
@@ -48,7 +48,7 @@ project "App"
             "{COPYFILE} ../Engine/Vendor/assimp/bin/assimp-vc143-mt.dll %{cfg.targetdir}/",
             "{COPYFILE} ../Engine/Vendor/glfw/lib-vc2022/*.dll %{cfg.targetdir}/",
             "\"$(SolutionDir)Scripts\\Content-Pipeline-Script.bat\" \"$(ProjectDir)\\Assets\\Textures\" \"$(SolutionDir)Bin\"",
-            "{COPY} ./Assets %{cfg.targetdir}/Assets
+            "{COPY} ./Assets %{cfg.targetdir}/Assets"
         }
 
     filter "configurations:Release"
